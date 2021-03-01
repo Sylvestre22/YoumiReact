ab9c74b7aa1a173374e8cc327174042b90cb9f14
Xavier Nouaille <xavier-nouaille@lyknowledge.io>
2021-03-01 11:37:20 +0100

    📦 NEW: Register Functionnality

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,27 +1,47 @@
-import React, {FunctionComponent as FC} from 'react';
+import React, {ChangeEvent, FormEvent, FunctionComponent as FC, useState} from 'react';
 import { HashLink } from 'react-router-hash-link';
 import axios from 'axios';
 
+interface IRegister{
+    name: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+}
+
 const Register:FC = ():JSX.Element =>{
+    const [data, setData] = useState<IRegister>({name: '', email: '', password: '', confirmPassword: ''})
+
+    const submit = (e: FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        console.log(data)
+    }
+
+    const change = (e: FormEvent<HTMLInputElement>)=>{
+        const { name,value } = e.currentTarget
+        setData({...data, [name]: value})
+    }
+
     return(
         <section className="register-wrapper">
             <h2>Inscription</h2>
-            <form className="d-flex flex-column my-5">
+            <form onSubmit={submit} className="d-flex flex-column my-5">
                 <label>
                     <p>Name</p>
-                    <input type="text"/>
+                    <input name="name" onChange={change} type="text"/>
+                    {data.name}
                 </label>
                 <label>
-                    <p>Username</p>
-                    <input type="text"/>
+                    <p>Email</p>
+                    <input name="email" onChange={change} value={data.email} type="text"/>
                     </label>
                 <label>
                     <p>Password</p>
-                    <input type="password"/>
+                    <input name="password" onChange={change} value={data.password} type="password"/>
                 </label>
                 <label>
                     <p>Confirm Password</p>
-                    <input type="password"/>
+                    <input name="confirmPassword" onChange={change} value={data.confirmPassword} type="password"/>
                 </label>
                 <div>
                     <button type="submit">Submit</button>
